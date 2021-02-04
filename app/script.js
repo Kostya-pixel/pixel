eff2ff04cefc1fee49adfa4ba33db0ec5eaa3fb5
Konstantin <konstantinpavlovich1987@gmail.com>
2021-02-04 14:53:34 +0200

    add section foryou

---
+++ app/script.js
@@ -0,0 +1,18 @@
+let socialBlock = document.querySelector('.link-social');
+let btnLink = document.querySelector('.link-off');
+let arrowOff = document.querySelector('.off-arrow');
+let arrowOn = document.querySelector('.on-arrow');
+
+    btnLink.onclick = function() {
+        if (socialBlock.style.left =='-70px') {
+            socialBlock.style.left = '0px';
+            arrowOff.style.display = 'block';
+            arrowOn.style.display = 'none';
+        }
+        else {
+            socialBlock.style.left = '-70px';
+            arrowOn.style.display = 'block';
+            arrowOff.style.display = 'none';
+        }
+        
+    }
